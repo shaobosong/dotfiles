72ac5b47fe7e4daa5280eebcb36f5ca6e2969200
Shaobo Song <shnusongshaobo@gmail.com>
2025-12-24 11:53:29 +0800

    update(neovim): Add window-switch keymap for terminal windows

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -10,6 +10,11 @@ vim.keymap.set('!', '<ESC>H', '<ESC><C-W>h', opts)
 vim.keymap.set('!', '<ESC>J', '<ESC><C-W>j', opts)
 vim.keymap.set('!', '<ESC>K', '<ESC><C-W>k', opts)
 
+vim.keymap.set('t', '<M-L>', '<C-\\><C-N><C-W>l', opts)
+vim.keymap.set('t', '<M-H>', '<C-\\><C-N><C-W>h', opts)
+vim.keymap.set('t', '<M-J>', '<C-\\><C-N><C-W>j', opts)
+vim.keymap.set('t', '<M-K>', '<C-\\><C-N><C-W>k', opts)
+
 vim.keymap.set('n', 'gs', ':s@<c-r><c-w>@<c-r><c-w>@g<C-F>hhi', opts)
 vim.keymap.set('x', 'gs', ':s@@@g<C-F>hhi', opts)
 
